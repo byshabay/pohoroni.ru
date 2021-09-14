cb4448f574ffa27d5fa94d3f3e799c15b1fc4779
Alyona Shabaykina <shabbaykina@mail.ru>
2021-09-14 11:29:50 +0300

    Add directory and file structure
    Check the work of the commit body.

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="RU">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Главная</title>
+
+    <!-- STYLE -->
+    <link rel="stylesheet" href="style.css">
+
+    <!-- SELF-WRITTEN SCRIPTS -->
+    <script src="scripts/script.js"></script>
+
+</head>
+
+<body>
+    <!-- 1.HEADER START -->
+
+    <!-- 1.HEADER END -->
+
+    <!-- 2.MAIN START -->
+
+    <!-- 2.MAIN END -->
+
+    <!-- 3.FOOTER START -->
+
+    <!-- 3.FOOTER END -->
+</body>
+
+</html>
