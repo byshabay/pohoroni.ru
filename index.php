2b642b88f7c6d8b176c000ab44117e56d51b62eb
Alyona Shabaykina <shabbaykina@mail.ru>
2021-09-16 16:50:08 +0300

    Add tariff block

--- index.php
+++ index.php
@@ -107,6 +107,7 @@
             <p class="common-p procedure__p">
                 Высокий уровень вовлечения представителей целевой аудитории является четким доказательством простого факта: сплочённость команды профессионалов влечет за собой процесс внедрения и модернизации глубокомысленных рассуждений.
             </p>
+            <a class="procedure__order-btn common-btn_peach" href="#">Заказать звонок</a>
         </div>
 
         <!-- 2.4.1.SLIDER START -->
@@ -140,6 +141,7 @@
                     <p class="common-p">
                         Высокий уровень вовлечения представителей целевой аудитории является четким доказательством простого факта: сплочённость команды профессионалов влечет за собой процесс внедрения и модернизации глубокомысленных рассуждений.
                     </p>
+
                 </div>
 
                 <!-- 2 SLIDE -->
@@ -190,6 +192,187 @@
     </section>
 
     <!-- 2.4.PROCEDURE END -->
+
+    <!-- 3.PRICES START -->
+
+    <section class="prices container">
+
+        <div class="prices__h2 common-h2">
+            ЦЕНЫ НА РИТУАЛЬНЫЕ УСЛУГИ
+        </div>
+
+        <!-- 3.1.TABS OF PRICES START -->
+
+        <ul class="prices__tabs">
+            <li class="prices__tab">
+                <a href="#price-1" class="prices__active-tab">Захоронение</a>
+                <hr class="prices__active-tab">
+            </li>
+            <li class="prices__tab">
+                <a href="#price-2">Кремация</a>
+                <hr>
+            </li>
+        </ul>
+
+        <!-- 3.1.TABS OF PRICES END -->
+
+        <!-- 3.2.PRICES TARIFFS START -->
+
+        <!-- BURIAL -->
+        <div id="price-1" class="prices__tariffs active-tariff">
+
+            <!-- 1 TARIFF -->
+
+            <div class="prices__tariff ">
+                <div class="prices__tariff-h2">
+                    Базовый
+                </div>
+
+                <p class="common-p prices__p">Гроб деревянный и ритуальные пренадлежности</p>
+                <p class="common-p prices__p">Выезд ритуального агента</p>
+                <p class="common-p prices__p">Транспорт (2 пос. места)</p>
+                <p class="common-p prices__p">Доставка принадлежностей в морг</p>
+                <p class="common-p prices__p">Заказ катафального транспорта</p>
+
+                <a class="prices__order-btn common-btn_peach" href="#">Заказать пакет</a>
+                <a class="prices__more-btn" href="#">Узнать подробнее</a>
+            </div>
+
+            <!-- 2 TARIFF -->
+
+            <div class="prices__tariff ">
+                <div class="prices__tariff-h2">
+                    Стандарт
+                </div>
+
+                <p class="common-p prices__p">Гроб деревянный и ритуальные пренадлежности</p>
+                <p class="common-p prices__p">Выезд ритуального агента</p>
+                <p class="common-p prices__p">Лента траурная</p>
+                <p class="common-p prices__p">Транспорт (6 пос. места)</p>
+                <p class="common-p prices__p">Доставка принадлежностей в морг</p>
+                <p class="common-p prices__p">Заказ катафального транспорта</p>
+
+                <a class="prices__order-btn common-btn_peach" href="#">Заказать пакет</a>
+                <a class="prices__more-btn" href="#">Узнать подробнее</a>
+            </div>
+
+            <!-- 3 TARIFF -->
+
+            <div class="prices__tariff ">
+                <div class="prices__tariff-h2">
+                    Премиум
+                </div>
+
+                <p class="common-p prices__p">Гроб деревянный и ритуальные пренадлежности</p>
+                <p class="common-p prices__p">Выезд ритуального агента</p>
+                <p class="common-p prices__p">Лента траурная заказная</p>
+                <p class="common-p prices__p">Транспорт (16 пос. места)</p>
+                <p class="common-p prices__p">Доставка принадлежностей в морг</p>
+                <p class="common-p prices__p">Заказ катафального транспорта</p>
+
+                <a class="prices__order-btn common-btn_peach" href="#">Заказать пакет</a>
+                <a class="prices__more-btn" href="#">Узнать подробнее</a>
+            </div>
+
+            <!-- 4 TARIFF -->
+
+            <div class="prices__tariff ">
+                <div class="prices__tariff-h2">
+                    VIP
+                </div>
+
+                <p class="common-p prices__p">Гроб деревянный и ритуальные пренадлежности</p>
+                <p class="common-p prices__p">Выезд ритуального агента</p>
+                <p class="common-p prices__p">Транспорт (16 пос. места)</p>
+                <p class="common-p prices__p">Доставка принадлежностей в морг</p>
+                <p class="common-p prices__p">Заказ катафального транспорта</p>
+                <p class="common-p prices__p">Бригада похоронного обслуживания</p>
+
+                <a class="prices__order-btn common-btn_peach" href="#">Заказать пакет</a>
+                <a class="prices__more-btn" href="#">Узнать подробнее</a>
+            </div>
+
+        </div>
+
+        <!-- CREMATION -->
+        <div id="price-2" class="prices__tariffs">
+            <!-- 1 TARIFF -->
+
+            <div class="prices__tariff ">
+                <div class="prices__tariff-h2">
+                    1 тариф
+                </div>
+
+                <p class="common-p prices__p">Гроб деревянный и ритуальные пренадлежности</p>
+                <p class="common-p prices__p">Выезд ритуального агента</p>
+                <p class="common-p prices__p">Транспорт (2 пос. места)</p>
+                <p class="common-p prices__p">Доставка принадлежностей в морг</p>
+                <p class="common-p prices__p">Заказ катафального транспорта</p>
+
+                <a class="prices__order-btn common-btn_peach" href="#">Заказать пакет</a>
+                <a class="prices__more-btn" href="#">Узнать подробнее</a>
+            </div>
+
+            <!-- 2 TARIFF -->
+
+            <div class="prices__tariff ">
+                <div class="prices__tariff-h2">
+                    2 тариф
+                </div>
+
+                <p class="common-p prices__p">Гроб деревянный и ритуальные пренадлежности</p>
+                <p class="common-p prices__p">Выезд ритуального агента</p>
+                <p class="common-p prices__p">Лента траурная</p>
+                <p class="common-p prices__p">Транспорт (6 пос. места)</p>
+                <p class="common-p prices__p">Доставка принадлежностей в морг</p>
+                <p class="common-p prices__p">Заказ катафального транспорта</p>
+
+                <a class="prices__order-btn common-btn_peach" href="#">Заказать пакет</a>
+                <a class="prices__more-btn" href="#">Узнать подробнее</a>
+            </div>
+
+            <!-- 3 TARIFF -->
+
+            <div class="prices__tariff ">
+                <div class="prices__tariff-h2">
+                    3 тариф
+                </div>
+
+                <p class="common-p prices__p">Гроб деревянный и ритуальные пренадлежности</p>
+                <p class="common-p prices__p">Выезд ритуального агента</p>
+                <p class="common-p prices__p">Лента траурная заказная</p>
+                <p class="common-p prices__p">Транспорт (16 пос. места)</p>
+                <p class="common-p prices__p">Доставка принадлежностей в морг</p>
+                <p class="common-p prices__p">Заказ катафального транспорта</p>
+
+                <a class="prices__order-btn common-btn_peach" href="#">Заказать пакет</a>
+                <a class="prices__more-btn" href="#">Узнать подробнее</a>
+            </div>
+
+            <!-- 4 TARIFF -->
+
+            <div class="prices__tariff ">
+                <div class="prices__tariff-h2">
+                    4 тариф
+                </div>
+
+                <p class="common-p prices__p">Гроб деревянный и ритуальные пренадлежности</p>
+                <p class="common-p prices__p">Выезд ритуального агента</p>
+                <p class="common-p prices__p">Транспорт (16 пос. места)</p>
+                <p class="common-p prices__p">Доставка принадлежностей в морг</p>
+                <p class="common-p prices__p">Заказ катафального транспорта</p>
+                <p class="common-p prices__p">Бригада похоронного обслуживания</p>
+
+                <a class="prices__order-btn common-btn_peach" href="#">Заказать пакет</a>
+                <a class="prices__more-btn" href="#">Узнать подробнее</a>
+            </div>
+        </div>
+        <!-- 3.2.PRICES TARIFFS END -->
+
+
+    </section>
+
+    <!-- 3.PRICES END -->
 </main>
 
 <!-- 2.MAIN END -->
--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,31 @@ $(document).ready(function () {
 
     // 1.HEADER SCROLL END
 
+    // 2.TARIFFS TABS START
+
+    $('.prices__tab a').click(function () {
+
+        $('.prices__active-tab').removeClass('prices__active-tab');
+        $(this).addClass('prices__active-tab');
+
+        $(this).siblings('hr').addClass('prices__active-tab');
+
+        let href = $(this).attr('href');
+        $('.active-tariff').removeClass('active-tariff');
+        $(href).addClass('active-tariff');
+
+
+        /**
+         * Код ниже нужен для того, чтобы страница не пролистовалась
+         * при нажатии на ссылку-якорь
+         * 
+         */
+
+        return false;
+    });
+
+    // 2.TARIFFS TABS END
+
 
 
 });
