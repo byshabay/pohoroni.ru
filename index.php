2683244c27b99485083518a14057808756d40c45
Alyona Shabaykina <shabbaykina@mail.ru>
2021-09-17 16:20:38 +0300

    Add contacts block for desktop

--- index.php
+++ index.php
@@ -547,6 +547,7 @@
     <!-- 2.8.LICENSES END -->
 
     <!-- 2.9.REVIEWS START -->
+
     <section class="reviews">
         <div class="container">
             <div class="common-h2 reviews__h2">Отзывы наших клиентов</div>
@@ -617,8 +618,43 @@
         </div>
     </section>
 
-
     <!-- 2.9.REVIEWS END -->
+
+    <!-- 2.10.CONTACTS START -->
+    <section class="contacts container">
+        <div class="contacts__map">
+            <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A91c02b928902c24807d8a070dc0c10cc80f6ba9e8ed237dee2659f2f8c728a74&amp;width=550&amp;height=550&amp;lang=ru_RU&amp;scroll=true"></script>
+        </div>
+        <div class="contacts__wrapper">
+            <div class="contacts__h2 common-h2">
+                Наши контакты
+            </div>
+            <a class="contacts__item" href="tel:+74957953080">
+                <img src="images/phone-call.png">
+                +7 495 795 30-80
+            </a>
+            <a class="contacts__item" href="mailto:info@mfc-ritual.ru">
+                <img src="images/email.png">
+                info@mfc-ritual.ru
+            </a>
+            <div class="contacts__item" class="contacts__address">
+                <img src="images/place.png">
+                Москва, м. Бульвар Дмитрия Донского, ул. 2-я Мелитопольская, 4
+            </div>
+            <p class="common-p contacts__p">
+                Закажите звонок и наши менеджера бесплатно прокконсультируют вас по любому вопросу.
+                <br>
+                Попросите список документов, которы нужно собрать для подачи заявки.
+            </p>
+            <a href="#" class="common-btn_peach contacts__btn">
+                Заказать звонок
+            </a>
+
+        </div>
+
+    </section>
+
+    <!-- 2.10.CONTACTS END -->
 </main>
 
 <!-- 2.MAIN END -->
