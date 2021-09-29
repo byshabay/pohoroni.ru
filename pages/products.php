c1d40758015e09a3aebe36d43f0e0a95399f85d6
Alyona Shabaykina <shabbaykina@mail.ru>
2021-09-29 11:10:19 +0300

    Add products page for desctop (without tabs and any fuctional)

---
+++ pages/products.php
@@ -0,0 +1,261 @@
+<!-- 1.HEADER START -->
+<?php include('../parts/header.php') ?>
+<!-- 1.HEADER END -->
+
+<!-- 2.MAIN START -->
+<main class="products main page">
+
+    <!-- 2.1.BREAD CRUMBS START -->
+
+    <ul class="bread-crumbs container">
+        <li class="bread-crumbs__item"><a href="<?= $root__url ?>">Главная</a></li>
+        <li class="bread-crumbs__item bread-crumbs__item_active"><a href="<?= $root__url ?>pages/products.php">Ритуальные товары</a></li>
+    </ul>
+
+    <!-- 2.1.BREAD CRUMBS END -->
+
+    <!-- 2.2.CATALOG START -->
+
+    <section class="catalog container">
+        <h1 class="common-h1 catalog__h1">
+            Каталог ритуальных товаров
+        </h1>
+
+        <div class="catalog__wrapper">
+            <div class="catalog__sidebar">
+                <div class="common-h2 catalog__h2">
+                    Категории
+                </div>
+                <ul class="catalog__sidebar-items">
+                    <li class="catalog__sidebar-item">
+                        <a href="#coffins">
+                            Гробы
+                            <div class="catalog__sidebar-item-count">
+                                76 товаров
+                            </div>
+                        </a>
+
+                        <ul class="catalog__sidebar-submenu">
+                            <li class="catalog__sidebar-submenu-item">
+                                <a href="#">
+                                    Постель в гроб
+                                </a>
+                            </li>
+                            <li class="catalog__sidebar-submenu-item">
+                                <a href="#">
+                                    Одежда
+                                </a>
+                            </li>
+                            <li class="catalog__sidebar-submenu-item">
+                                <a href="#">
+                                    Покрывало
+                                </a>
+                            </li>
+                            <li class="catalog__sidebar-submenu-item">
+                                <a href="#">
+                                    Подушка в гроб
+                                </a>
+                            </li>
+                            <li class="catalog__sidebar-submenu-item">
+                                <a href="#">
+                                    Обувь
+                                </a>
+                            </li>
+                            <li class="catalog__sidebar-submenu-item">
+                                <a href="#">
+                                    Орденские подушки
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="catalog__sidebar-item">
+                        <a href="#wreath">
+                            Венки
+                            <div class="catalog__sidebar-item-count">
+                                76 товаров
+                            </div>
+                        </a>
+                    </li>
+                    <li class="catalog__sidebar-item">
+                        <a href="#textile">
+                            Текстиль
+                            <div class="catalog__sidebar-item-count">
+                                76 товаров
+                            </div>
+                        </a>
+                    </li>
+                    <li class="catalog__sidebar-item">
+                        <a href="#crosses">
+                            Кресты
+                            <div class="catalog__sidebar-item-count">
+                                76 товаров
+                            </div>
+                        </a>
+                    </li>
+                    <li class="catalog__sidebar-item">
+                        <a href="#accessories">Аксессуары
+                            <div class="catalog__sidebar-item-count">
+                                76 товаров
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <ul class="catalog__products">
+
+                <!-- 1 PRODUCT -->
+
+                <li class="catalog__simple-product">
+                    <img src="../images/product-img.png">
+                    <div class="catalog__product-info-box">
+                        <a href="#" class="catalog__product-name">
+                            Покрывало
+                            тюль ритуальный
+                        </a>
+                        <p class="catalog__in-stock">
+                            в наличии
+                        </p>
+                        <p class="catalog__price">
+                            1464 ₽
+                        </p>
+                        <a href="#" class="common-border-button catalog__to-order-btn">
+                            Заказать
+                        </a>
+                    </div>
+                </li>
+
+                <!-- 2 PRODUCT -->
+
+                <li class="catalog__simple-product">
+                    <img src="../images/product-img.png">
+                    <div class="catalog__product-info-box">
+                        <a href="#" class="catalog__product-name">
+                            Покрывало
+                            тюль ритуальный
+                        </a>
+                        <p class="catalog__in-stock">
+                            в наличии
+                        </p>
+                        <p class="catalog__price">
+                            1464 ₽
+                        </p>
+                        <a href="#" class="common-border-button catalog__to-order-btn">
+                            Заказать
+                        </a>
+                    </div>
+                </li>
+
+                <!-- 3 PRODUCT -->
+
+                <li class="catalog__simple-product">
+                    <img src="../images/product-img.png">
+                    <div class="catalog__product-info-box">
+                        <a href="#" class="catalog__product-name">
+                            Покрывало
+                            тюль ритуальный
+                        </a>
+                        <p class="catalog__in-stock">
+                            в наличии
+                        </p>
+                        <p class="catalog__price">
+                            1464 ₽
+                        </p>
+                        <a href="#" class="common-border-button catalog__to-order-btn">
+                            Заказать
+                        </a>
+                    </div>
+                </li>
+
+
+                <!-- 4 PRODUCT -->
+
+                <li class="catalog__simple-product">
+                    <img src="../images/product-img.png">
+                    <div class="catalog__product-info-box">
+                        <a href="#" class="catalog__product-name">
+                            Покрывало
+                            тюль ритуальный
+                        </a>
+                        <p class="catalog__in-stock">
+                            в наличии
+                        </p>
+                        <p class="catalog__price">
+                            1464 ₽
+                        </p>
+                        <a href="#" class="common-border-button catalog__to-order-btn">
+                            Заказать
+                        </a>
+                    </div>
+                </li>
+
+                <!-- 5 PRODUCT -->
+
+                <li class="catalog__simple-product">
+                    <img src="../images/product-img.png">
+                    <div class="catalog__product-info-box">
+                        <a href="#" class="catalog__product-name">
+                            Покрывало
+                            тюль ритуальный
+                        </a>
+                        <p class="catalog__in-stock">
+                            в наличии
+                        </p>
+                        <p class="catalog__price">
+                            1464 ₽
+                        </p>
+                        <a href="#" class="common-border-button catalog__to-order-btn">
+                            Заказать
+                        </a>
+                    </div>
+                </li>
+
+                <!-- 6 PRODUCT -->
+
+                <li class="catalog__simple-product">
+                    <img src="../images/product-img.png">
+                    <div class="catalog__product-info-box">
+                        <a href="#" class="catalog__product-name">
+                            Покрывало
+                            тюль ритуальный
+                        </a>
+                        <p class="catalog__in-stock">
+                            в наличии
+                        </p>
+                        <p class="catalog__price">
+                            1464 ₽
+                        </p>
+                        <a href="#" class="common-border-button catalog__to-order-btn">
+                            Заказать
+                        </a>
+                    </div>
+                </li>
+
+
+            </ul>
+        </div>
+    </section>
+
+
+    <!-- 2.2.CATALOG END -->
+
+    <!-- 2.3.HAVE A QUESTION START -->
+
+    <?php include('../parts/have-a-question.php') ?>
+
+    <!-- 2.3.HAVE A QUESTION END -->
+
+    <!-- 2.4.CONTACT START -->
+
+    <?php include('../parts/contact-part.php') ?>
+
+    <!-- 2.4.CONTSCT END -->
+
+</main>
+
+<!-- 2.MAIN END -->
+
+<!-- 3.FOOTER START -->
+<?php
+include('../parts/footer.php')
+?>
+<!-- 3.FOOTER END -->
